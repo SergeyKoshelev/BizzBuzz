3558e055df24fc23f68b68792c66460a4abcfd71
SergeyKoshelev <Koshelev.SM@phystech.edu>
2020-10-02 00:04:04 +0300

    case of 1 argument

--- bzbz_file.c
+++ bzbz_file.c
@@ -7,33 +7,45 @@
 
 int main(int argc, char** argv)
 {
-	if (argc != 3)
+    int fd_src, fd_dir;
+	if (argc == 2)
+	{
+        fd_src = open(argv[1], O_RDONLY);
+        fd_dir = open("output", O_RDWR);
+        if (fd_dir >= 0)
+            unlink("output");
+        fd_dir = open("output", O_CREAT | O_RDWR, 0666);
+	}
+	else if (argc == 3)
+	{
+        fd_src = open(argv[1], O_RDONLY);
+        fd_dir = open(argv[2], O_RDWR);
+        if (fd_dir >= 0)
+            unlink(argv[2]);
+        fd_dir = open(argv[2], O_CREAT | O_RDWR, 0666);
+	}
+	else
 	{
 		printf("Incorrect count of arguments\n");
 		exit(0);
 	}
-	int fd_src = open(argv[1], O_RDONLY);
-	if (fd_src < 0)
+
+	if ((fd_src < 0)||(fd_dir < 0))
 	{
-		printf("Error with reading\n");
+		printf("Error with opening\n");
 		exit(1);
 	}
 
-	struct stat* fd_stat = malloc(sizeof(struct stat));
-	fstat(fd_src, fd_stat);
-	off_t size_src = fd_stat->st_size;
+	struct stat* fd_stat = malloc (sizeof(struct stat));
+        fstat(fd_src, fd_stat);
+        off_t size_src = fd_stat->st_size;
 	if (((fd_stat->st_mode) & S_IFMT) != S_IFREG)
 	{
-		printf("Not a regular file\n");
+		printf ("Not a regular file\n");
 		free(fd_stat);
 		close(fd_src);
 		exit(5);
 	}
-	int fd_dir = open(argv[2], O_RDWR);
-	if (fd_dir >= 0)
-		unlink(argv[2]);
-	fd_dir = open(argv[2], O_CREAT | O_RDWR, 0666);
-
 
 	void* temp = malloc(1);
 	int last_is_5 = 0;
@@ -49,54 +61,54 @@ int main(int argc, char** argv)
 	for (off_t i = 0; i < size_src; i++)
 	{
 		prev_symbol = *(char*)temp;
-		read_size = read(fd_src, temp, 1);
+		read_size = read (fd_src, temp, 1);
 		if (read_size != 1)
 		{
-			printf("Error with reading symbol\n");
-			exit(3);
+			printf ("Error with reading symbol\n");
+			exit (3);
 		}
 
-		if ((*(char*)temp == ' ') || (i == (size_src - 1)) || (*(char*)temp == '\n') || (*(char*)temp == '\t'))
+		if ((*(char*)temp == ' ')||(i == (size_src - 1))||(*(char*)temp == '\n')||(*(char*)temp == '\t'))
 		{
 			if (last_is_5 && (sum == 0) && check_is_num)
 			{
-				if (prev_sep != '0')
-					write(fd_dir, &prev_sep, 1);
-				write(fd_dir, &"bizzbuzz", 8);
-			}
+                if (prev_sep != '0')
+                    write (fd_dir, &prev_sep, 1);
+				write (fd_dir, &"bizzbuzz", 8);
+            }
 			else if (last_is_5 && check_is_num)
 			{
-				if (prev_sep != '0')
-					write(fd_dir, &prev_sep, 1);
-				write(fd_dir, &"buzz", 4);
-			}
+                if (prev_sep != '0')
+                    write (fd_dir, &prev_sep, 1);
+				write (fd_dir, &"buzz", 4);
+            }
 			else if ((sum == 0) && check_is_num)
 			{
-				if (prev_sep != '0')
-					write(fd_dir, &prev_sep, 1);
-				write(fd_dir, &"bizz", 4);
-			}
+                if (prev_sep != '0')
+                    write (fd_dir, &prev_sep, 1);
+				write (fd_dir, &"bizz", 4);
+            }
 			else
 			{
-				lseek(fd_src, prev_off, SEEK_SET);
+				lseek (fd_src, prev_off, SEEK_SET);
 				if (prev_sep != '0')
-					write(fd_dir, &prev_sep, 1);
+                    write (fd_dir, &prev_sep, 1);
 
 				if (negative)
-					write(fd_dir, &"-", 1);
+					write (fd_dir, &"-", 1);
 				for (off_t j = 0; j < i - prev_off; j++)
 				{
-					read(fd_src, temp, 1);
-					write(fd_dir, temp, 1);
+					read (fd_src, temp, 1);
+					write (fd_dir, temp, 1);
 				}
 				read(fd_src, temp, 1);
 			}
-			prev_off = i + 1;
-			last_is_5 = 0;
-			sum = -3;
-			check_is_num = 1;
-			negative = 0;
-			prev_sep = *(char*)temp;
+		prev_off = i + 1;
+		last_is_5 = 0;
+		sum = -3;
+		check_is_num = 1;
+		negative = 0;
+		prev_sep = *(char*)temp;
 
 		}
 		else
@@ -104,7 +116,7 @@ int main(int argc, char** argv)
 			number = *(char*)temp - 48;
 			if (*(char*)temp == '-')
 			{
-				if ((prev_symbol == ' ') || (i == 0) || (prev_symbol == '\n') || (prev_symbol == '\t'))
+				if ((prev_symbol == ' ')||(i == 0)||(prev_symbol == '\n')||(prev_symbol == '\t'))
 				{
 					negative = 1;
 					prev_off++;
@@ -112,7 +124,7 @@ int main(int argc, char** argv)
 				else
 					check_is_num = 0;
 			}
-			else if ((number < 0) || (number > 9))
+			else if ((number < 0)||(number > 9))
 				check_is_num = 0;
 			else
 			{
@@ -126,7 +138,7 @@ int main(int argc, char** argv)
 		}
 	}
 
-	write(fd_dir, &"\n", 1);
+	write (fd_dir,&"\n", 1);
 	close(fd_src);
 	close(fd_dir);
 	free(temp);
