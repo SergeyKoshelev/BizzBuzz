5d226e7367cfed5bc2ade619c6190b8d038b0d27
SergeyKoshelev <Koshelev.SM@phystech.edu>
2020-11-27 11:43:56 +0300

    20.11, add many events for inotify

--- SimplePrograms/Inotify/inotify.c
+++ SimplePrograms/Inotify/inotify.c
@@ -4,38 +4,40 @@
 #include <unistd.h>
 #include <assert.h>
 
+const int arr_size = 100;
 
 int main(int argc, char** argv)
 {
-    int in, watch, ret;
+    int in, watch;
+    size_t count;
     int mask = IN_CREATE | IN_DELETE | IN_MOVE_SELF;
-    char buf[4096];
-    struct inotify_event* event; 
+    struct inotify_event events[arr_size]; 
 
     assert(argc > 1);
 
     in = inotify_init();
     assert (in >= 0);
-
+    
+    
     watch = inotify_add_watch(in, argv[1], mask);
     assert (watch >= 0);
     
     while (1)
     {
-        ret = read(in, buf, 4096);
-        if (ret > 0)
+        count = read(in, (void*)events, sizeof(struct inotify_event) * arr_size) / sizeof(struct inotify_event);
+        for (int i = 0; i < count - 1; i++)
         {
-            event = buf;
-            printf("path: %s  ", event->name);
+            printf("%d   path: %s  ", i, events[i].name);
 
-            if (event->mask & IN_CREATE)
+            if (events[i].mask & IN_CREATE)
                 printf("event: create\n");
-            else if (event->mask & IN_DELETE)
+            else if (events[i].mask & IN_DELETE)
                 printf("event: delete\n");
-            else if (event->mask & IN_MOVE_SELF)
+            else if (events[i].mask & IN_MOVE_SELF)
                 printf("event: move\n");
             else
                 printf("UNKNOWN\n");
         }
+        printf("\n");
     }
 }
